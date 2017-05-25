28d70cecfda9062265c38a7f209e7055703b5abd
Romain Demoustier <rdemoustier@hotmail.com>
2017-05-25 10:30:32 +0200

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create({
+  name: "Pistolei",
+  address: "Rue de la bourse",
+  phone_number: "027834524",
+  category: "belgian"   })
+
+Restaurant.create({
+  name: "Sushi shop",
+  address: "Rue de la rivière",
+  phone_number: "027863224",
+  category: "japanese"   })
+
+Restaurant.create({
+  name: "Chang",
+  address: "Rue du fleuve",
+  phone_number: "027831224",
+  category: "chinese"   })
+
+Restaurant.create({
+  name: "Pizza pizza",
+  address: "Rue de la mer",
+  phone_number: "097834524",
+  category: "italian"   })
+
+Restaurant.create({
+  name: "Hachis Parmentier",
+  address: "Rue du ruisseau",
+  phone_number: "037834524",
+  category: "french"   })
